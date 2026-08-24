d085fc0804a5639d2d8fbaafa890b8a86238b27b
Felipe Sestelo <fsestelo@gmail.com>
2026-08-23 23:35:09 -0300

    :recycle: Refactoring code according to baseline - step 4: Extract validation logic (:white_check_mark: pytest passed)

---
+++ starter/sudoku/validation.py
@@ -0,0 +1,16 @@
+from sudoku.constants import SIZE
+
+
+def is_safe(board, row, col, num):
+    # Check row and column
+    for x in range(SIZE):
+        if board[row][x] == num or board[x][col] == num:
+            return False
+    # Check 3x3 box
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+    return True
--- starter/sudoku_logic.py
+++ starter/sudoku_logic.py
@@ -3,6 +3,7 @@ import random
 from sudoku.board import create_empty_board as _create_empty_board
 from sudoku.board import deep_copy as _deep_copy
 from sudoku.constants import EMPTY, SIZE
+from sudoku.validation import is_safe as _is_safe
 
 
 def deep_copy(board):
@@ -12,19 +13,9 @@ def deep_copy(board):
 def create_empty_board():
     return _create_empty_board()
 
+
 def is_safe(board, row, col, num):
-    # Check row and column
-    for x in range(SIZE):
-        if board[row][x] == num or board[x][col] == num:
-            return False
-    # Check 3x3 box
-    start_row = row - row % 3
-    start_col = col - col % 3
-    for i in range(3):
-        for j in range(3):
-            if board[start_row + i][start_col + j] == num:
-                return False
-    return True
+    return _is_safe(board, row, col, num)
 
 def fill_board(board):
     for row in range(SIZE):
