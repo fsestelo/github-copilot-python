2232e5e33fd23d2539fa07f6134d21d6ae496165
Felipe Sestelo <fsestelo@gmail.com>
2026-08-24 15:25:14 -0300

    :recycle: Refactoring code according to baseline - step 8: Modularize puzzle generation (:white_check_mark: pytest passed)

--- starter/sudoku/generator.py
+++ starter/sudoku/generator.py
@@ -1,6 +1,8 @@
 import random
 
+from sudoku.board import create_empty_board, deep_copy
 from sudoku.constants import EMPTY, SIZE
+from sudoku.solver import fill_board
 
 
 def remove_cells(board, clues):
@@ -11,3 +13,12 @@ def remove_cells(board, clues):
         if board[row][col] != EMPTY:
             board[row][col] = EMPTY
             attempts -= 1
+
+
+def generate_puzzle(clues=35):
+    board = create_empty_board()
+    fill_board(board)
+    solution = deep_copy(board)
+    remove_cells(board, clues)
+    puzzle = deep_copy(board)
+    return puzzle, solution
--- starter/sudoku_logic.py
+++ starter/sudoku_logic.py
@@ -3,6 +3,7 @@ import random
 from sudoku.board import create_empty_board as _create_empty_board
 from sudoku.board import deep_copy as _deep_copy
 from sudoku.constants import EMPTY, SIZE
+from sudoku.generator import generate_puzzle as _generate_puzzle
 from sudoku.generator import remove_cells as _remove_cells
 from sudoku.solver import fill_board as _fill_board
 from sudoku.validation import is_safe as _is_safe
@@ -29,9 +30,4 @@ def remove_cells(board, clues):
 
 
 def generate_puzzle(clues=35):
-    board = create_empty_board()
-    fill_board(board)
-    solution = deep_copy(board)
-    remove_cells(board, clues)
-    puzzle = deep_copy(board)
-    return puzzle, solution
+    return _generate_puzzle(clues)
